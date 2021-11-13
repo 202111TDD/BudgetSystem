e0cb839c5fb4b4b67f62e5bae227a01b1985dc68
Joey Chen <jay.hatelove@gmail.com>
2021-11-14 00:21:07 +0800

    clean up code

--- src/main/java/com/example/demo/Budget.java
+++ src/main/java/com/example/demo/Budget.java
@@ -14,47 +14,39 @@ public class Budget {
         this.amount = amount;
     }
 
-    public int days() {
-        return getYearMonthFromBudget().lengthOfMonth();
+    public double overlappingAmount(Period period) {
+        return dailyAmount() * period.getOverlappingDays(createPeriod());
     }
 
-    public LocalDate firstDay() {
-        return getYearMonthFromBudget().atDay(1);
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public String getYearMonth() {
-        return yearMonth;
-    }
-
-    public void setYearMonth(String yearMonth) {
-        this.yearMonth = yearMonth;
-    }
-
-    public LocalDate lastDay() {
-        return getYearMonthFromBudget().atEndOfMonth();
-    }
-
-    Period createPeriod() {
+    private Period createPeriod() {
         return new Period(firstDay(), lastDay());
     }
 
-    double dailyAmount() {
+    private double dailyAmount() {
         return getAmount() / (double) days();
     }
 
-    YearMonth getYearMonthFromBudget() {
+    private int days() {
+        return getYearMonthFromBudget().lengthOfMonth();
+    }
+
+    private LocalDate firstDay() {
+        return getYearMonthFromBudget().atDay(1);
+    }
+
+    private int getAmount() {
+        return amount;
+    }
+
+    private String getYearMonth() {
+        return yearMonth;
+    }
+
+    private YearMonth getYearMonthFromBudget() {
         return YearMonth.parse(getYearMonth(), DateTimeFormatter.ofPattern("yyyyMM"));
     }
 
-    double overlappingAmount(Period period) {
-        return dailyAmount() * period.getOverlappingDays(createPeriod());
+    private LocalDate lastDay() {
+        return getYearMonthFromBudget().atEndOfMonth();
     }
 }
