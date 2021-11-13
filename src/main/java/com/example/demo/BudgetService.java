3b8f5390ded96731b45be76b2c4a957d99d1ebc4
Joey Chen <jay.hatelove@gmail.com>
2021-11-14 00:17:45 +0800

    clean up code

--- src/main/java/com/example/demo/BudgetService.java
+++ src/main/java/com/example/demo/BudgetService.java
@@ -20,20 +20,6 @@ public class BudgetService {
             return 0;
         }
 
-        YearMonth startYearMonth = YearMonth.from(startDate);
-        YearMonth endYearMonth = YearMonth.from(endDate);
-
-        // 起大於訖 return 0
-        // 年月一樣
-        // 這個月有幾天，算出"每天的預算"
-        // 算出"區間有幾天"
-        // 算出"區間預算"
-        // 年月不同
-        // 取得區間內所有 Budget
-        // 算出"每個budget的每天的預算"
-        // 算出區間內"各月的天數"
-        //"各月的天數" X "每個budget的每天的預算"
-
         List<Budget> budgets = budgetRepo.getAll();
         double totalAmount = 0;
         Period period = new Period(startDate, endDate);
@@ -41,22 +27,5 @@ public class BudgetService {
             totalAmount += budget.overlappingAmount(period);
         }
         return totalAmount;
-//        if (startYearMonth.equals(endYearMonth)) {
-//            Optional<Budget> budget = budgets.stream()
-//                    .filter(b -> startYearMonth.format(DateTimeFormatter.ofPattern("yyyyMM")).equals(b.getYearMonth()))
-//                    .findFirst();
-//
-//            double amount = budget.map(Budget::getAmount).orElse(0);
-//            double oneDayBudget = amount / (double) startYearMonth.lengthOfMonth();
-//            int day = endDate.getDayOfMonth() - startDate.getDayOfMonth() + 1;
-//            return oneDayBudget * day;
-//        } else {
-//            double totalAmount = 0;
-//            Period period = new Period(startDate, endDate);
-//            for (Budget budget : budgets) {
-//                totalAmount += budget.overlappingAmount(period);
-//            }
-//            return totalAmount;
-//        }
     }
 }
