6fe5bc2e0da3f6172a48c4b4685e5cc07e87d454
Joey Chen <jay.hatelove@gmail.com>
2021-11-13 22:58:54 +0800

    fix test

--- src/test/java/com/example/demo/BudgetSystemApplicationTests.java
+++ src/test/java/com/example/demo/BudgetSystemApplicationTests.java
@@ -2,76 +2,79 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class BudgetSystemApplicationTests {
 
-	@MockBean
-	IBudgetRepo budgetRepo;
+    @MockBean
+    IBudgetRepo budgetRepo;
 
-	@Autowired
-	BudgetService budgetService;
+    @Autowired
+    BudgetService budgetService;
 
-	@Test
-	void zero() {
+    @Test
+    void crossMonth() {
+        LocalDate s = LocalDate.parse("20211101", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        LocalDate e = LocalDate.parse("20211228", DateTimeFormatter.ofPattern("yyyyMMdd"));
 
-		when(budgetRepo.getAll()).thenReturn(Collections.emptyList());
+        when(budgetRepo.getAll()).thenReturn(asList(new Budget("202111", 3000), new Budget("202112", 6200)));
+        Assertions.assertEquals(3000 + 6200 - 600, budgetService.query(s, e));
+    }
 
-		Assertions.assertEquals(0,budgetService.query(LocalDate.now(), LocalDate.now()));
-	}
+    @Test
+    void errorDate() {
+        LocalDate s = LocalDate.parse("20211130", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        LocalDate e = LocalDate.parse("20211101", DateTimeFormatter.ofPattern("yyyyMMdd"));
 
-	@Test
-	void singleDay() {
-		when(budgetRepo.getAll()).thenReturn(Arrays.asList(new Budget("202111", 3000)));
-		Assertions.assertEquals(100,budgetService.query(LocalDate.now(), LocalDate.now()));
-	}
+        when(budgetRepo.getAll()).thenReturn(asList(new Budget("202111", 3000)));
+        Assertions.assertEquals(0, budgetService.query(s, e));
+    }
 
-	@Test
-	void fullMonth() {
-		LocalDate s = LocalDate.parse("20211101", DateTimeFormatter.ofPattern("yyyyMMdd"));
-		LocalDate e = LocalDate.parse("20211130", DateTimeFormatter.ofPattern("yyyyMMdd"));
+    @Test
+    void fullMonth() {
+        LocalDate s = LocalDate.parse("20211101", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        LocalDate e = LocalDate.parse("20211130", DateTimeFormatter.ofPattern("yyyyMMdd"));
 
-		when(budgetRepo.getAll()).thenReturn(Arrays.asList(new Budget("202111", 3000)));
-		Assertions.assertEquals(3000,budgetService.query(s, e));
-	}
+        when(budgetRepo.getAll()).thenReturn(asList(new Budget("202111", 3000)));
+        Assertions.assertEquals(3000, budgetService.query(s, e));
+    }
 
-	@Test
-	void crossMonth() {
-		LocalDate s = LocalDate.parse("20211101", DateTimeFormatter.ofPattern("yyyyMMdd"));
-		LocalDate e = LocalDate.parse("20211228", DateTimeFormatter.ofPattern("yyyyMMdd"));
+    @Test
+    void singleDay() {
+        when(budgetRepo.getAll()).thenReturn(asList(new Budget("202111", 3000)));
+        LocalDate start = LocalDate.of(2021, 11, 2);
+        LocalDate end = LocalDate.of(2021, 11, 2);
+        Assertions.assertEquals(100, budgetService.query(start, end));
+    }
 
-		when(budgetRepo.getAll()).thenReturn(Arrays.asList(new Budget("202111", 3000),new Budget("202112", 6200)));
-		Assertions.assertEquals(3000+6200-600,budgetService.query(s, e));
-	}
+    @Test
+    void threeMonth() {
+        LocalDate s = LocalDate.parse("20211031", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        LocalDate e = LocalDate.parse("20211204", DateTimeFormatter.ofPattern("yyyyMMdd"));
 
-	@Test
-	void threeMonth() {
-		LocalDate s = LocalDate.parse("20211101", DateTimeFormatter.ofPattern("yyyyMMdd"));
-		LocalDate e = LocalDate.parse("20221228", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        when(budgetRepo.getAll()).thenReturn(asList(
+                new Budget("202110", 31)
+                , new Budget("202111", 3000)
+                , new Budget("202112", 3100)
+        ));
+        Assertions.assertEquals(1 + 3000 + 100 * 4, budgetService.query(s, e));
+    }
 
+    @Test
+    void zero() {
 
-		when(budgetRepo.getAll()).thenReturn(Arrays.asList(new Budget("202111", 3000),new Budget("202112", 6200)));
-		Assertions.assertEquals(3000+6200,budgetService.query(s, e));
-	}
+        when(budgetRepo.getAll()).thenReturn(Collections.emptyList());
 
-	@Test
-	void errorDate() {
-		LocalDate s = LocalDate.parse("20211130", DateTimeFormatter.ofPattern("yyyyMMdd"));
-		LocalDate e = LocalDate.parse("20211101", DateTimeFormatter.ofPattern("yyyyMMdd"));
-
-		when(budgetRepo.getAll()).thenReturn(Arrays.asList(new Budget("202111", 3000)));
-		Assertions.assertEquals(0,budgetService.query(s, e));
-	}
+        Assertions.assertEquals(0, budgetService.query(LocalDate.now(), LocalDate.now()));
+    }
 }
